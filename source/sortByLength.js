b3a3d371c3ff5166850e3ade1ec3a7545f34dc34
Aleksandr Vindman <vindmanaleksandr@gmail.com>
2026-02-07 23:59:43 +0300

    dz_0 is done.

---
+++ source/sortByLength.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Функция, сортирующая массив строк по их длине, при одинаковой длине также по алфавиту
+ * @param {Array<String>} strings - массив строк
+ * 
+ * @example
+ * // returns ["a", "bb", "ccc"]
+ * sortByLength(["bb", "a", "ccc"]);
+ * 
+ * @returns {Array<String>}
+ */
+let sortByLength = (strings) => [...strings].sort( (a, b) => a.length - b.length || a.localeCompare(b) );
--- test/sortByLength.js
+++ test/sortByLength.js
@@ -18,4 +18,19 @@ QUnit.module("Тестируем функцию sortByLength", function() {
 
         assert.deepEqual(result, ["hello"], "Массив с одной строкой должен вернуть ту же строку.");
     });
+    QUnit.test("Правильно обрабатывает пустой массив", function(assert) {
+        const result = sortByLength([]);
+
+        assert.deepEqual(result, [], "Должна быть возвращена пустота.");
+    });
+    QUnit.test("Правильно обрабатывает две одинаковые строки.", function(assert) {
+        const result = sortByLength(["Chicago", "Chicago"]);
+
+        assert.deepEqual(result, ["Chicago", "Chicago"], "Должны быть возвращены две одинаковые строки.");
+    });
+    QUnit.test("Правильно обрабатывает буквы с повторениями.", function(assert) {
+        const result = sortByLength(["d", "a", "b", "c", "d"]);
+
+        assert.deepEqual(result, ["a", "b", "c", "d", "d"], "Должны быть возвращены буквы в алфавитном порядке.");
+    });
 });
